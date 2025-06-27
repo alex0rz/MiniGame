49ec4959f362028b5ea6b8bb4c97ac1a342df032
alex0rz <42833034+alex0rz@users.noreply.github.com>
2025-06-27 23:05:01 +0800

    Add files via upload

---
+++ GameInterface/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Abstract.Interface;
+using GameLogic.Manager;
+
+namespace GameInterface
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var initialSetting = new TurnProcessor.InitialSettingUps(10, 10, 10, 0, 0, 5, 1, 0, 0, 0, 0, WeatherType.Comfort);
+            var turnManager = new TurnProcessor(initialSetting);
+            Application.Run(new GameForm(turnManager));
+        }
+    }
+}
